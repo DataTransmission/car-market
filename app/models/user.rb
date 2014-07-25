ac825651e6becf3b32e5e77fecf5913d94ac5eb5
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-25 00:01:06 -0700

    add model for user and add some data validation to the model;

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+	validates(:first_name, presence: true, length: { maximum: 25 })
+	validates(:last_name, presence: true, length: { maximum: 25 })
+	validates(:nickname, presence: true, length: { maximum: 25 }, uniqueness: true)
+
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates(:email, 
+		presence: true, 
+		format: { with: VALID_EMAIL_REGEX }, 
+		uniqueness: { case_sensitive: false })
+
+	before_save { self.email = email.downcase }
+end
