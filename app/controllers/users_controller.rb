7087378288de6a6f3b541fabd41c97c39e39bed0
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-27 10:09:13 -0700

    update users_controller with create, destroy, and update methods;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	respond_to :json
+
 	def index
 		@users = User.all
 		render :json => @users.to_json
@@ -8,6 +10,39 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		render :json => @user.to_json
+	end
+
+	def create
+	# render :json => 'Sign Up API is reached...'.to_json
+	@user = User.new(user_params)
+		if @user.save # Handle a successful save.
+			render :json => @user.to_json
+		else
+			render :json => params[:action].to_json
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			render :json => 'Updating'.to_json
+		end
+	end
+
+
+	private
+	def user_params
+		params
+		.permit(:first_name, :last_name, :nickname, :email, :password, :password_confirmation)
 	end
 
 end
