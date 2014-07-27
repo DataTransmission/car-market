206a2081c801f205fa57482f40eb3469ce9489c8
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-27 10:11:55 -0700

    update migration file of user;

--- db/migrate/20140725024057_create_users.rb
+++ db/migrate/20140725024057_create_users.rb
@@ -1,5 +1,18 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  # def change
+  #   create_table :users do |t|
+  #     t.string :first_name
+  #     t.string :last_name
+  #     t.string :nickname
+  #     t.string :email
+  #     t.string :password_digest
+  #     t.string :status
+
+  #     t.timestamps
+  #   end
+  # end
+
+   def self.up
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
@@ -11,4 +24,9 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
   end
+
+  def self.down
+    drop_table :users
+  end
+
 end
