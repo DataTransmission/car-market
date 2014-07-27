7634fca7d91600966663a6c741559e65d372d39a
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-27 10:13:01 -0700

    update routes;

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  resources :users, :only => [:index, :show, :edit]
+  resources :users, :only => [:index, :create, :show, :edit]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
