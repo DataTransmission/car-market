96ea45d0f2bd9de99880bd2fce2735f7a1da67bb
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-29 01:07:54 -0700

    add sign in and sign out methods for users_controller;

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -47,7 +47,26 @@ class Api::V1::UsersController < ApplicationController
 		end
 	end
 
-	
+	def signin
+		token = ApiKey.create!
+		msg = { token: token.access_token }
+		respond_to do |format|
+			format.json { render json: msg }
+		end
+	end
+
+	def signout
+		authenticate_or_request_with_http_token do |token, options|
+			token = ApiKey.find_by(access_token: token)
+			if token != nil
+				token.destroy
+			end
+		end
+		msg = { status: "OK" }
+		respond_to do |format|
+			format.json { render json: msg }
+		end
+	end
 
 
 	private
