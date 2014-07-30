d8c068504c48761e37746f8d1623297bf1be04a2
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-30 00:43:09 -0700

    update model of api_key;

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,10 +1,12 @@
 class ApiKey < ActiveRecord::Base
 	before_create :generate_access_token
-	
+
 	validates :access_token, uniqueness: true
-	
+	validates :user_id, uniqueness: true
+
 	private
 	def generate_access_token
+		self.user_id = user_id
 		begin
 			self.access_token = SecureRandom.hex
 		end while self.class.exists?(access_token: access_token)
