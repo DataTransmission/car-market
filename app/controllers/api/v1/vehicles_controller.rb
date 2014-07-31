650dedd8738316bf49674c85cb51b299502203ae
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-31 01:02:57 -0700

    add fetch_vehicle method and before_filter for vehicles_controller;

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::VehiclesController < ApplicationController
+
+	before_filter :fetch_vehicle, :except => [:index, :create]
+
 	def index
 		@vehicle = Vehicle.all
 		respond_to do |format|
@@ -6,4 +9,8 @@ class Api::V1::VehiclesController < ApplicationController
 		end
 	end
 
+	private
+    def fetch_vehicle
+    	@vehicle = Vehicle.find_by_id(params[:id])
+    end
 end
