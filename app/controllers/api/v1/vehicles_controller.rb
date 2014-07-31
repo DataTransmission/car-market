5b2baf7b187f4e90c949afe884ad29531d6a5fa4
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-31 00:56:15 -0700

    add index method to vehicles_controller;

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::VehiclesController < ApplicationController
+	def index
+		@vehicle = Vehicle.all
+		respond_to do |format|
+			format.json { render json: @vehicle }
+		end
+	end
+
 end
