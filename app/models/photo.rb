632b1e2f727c8ba5db5f4356e26b82e2810a75df
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-02 00:07:02 -0700

    add attr_accessor for vehicle on model of photo;

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,13 @@
 class Photo < ActiveRecord::Base
 	has_one :user
 	has_one :vehicle
-	attr_accessor :user
+	attr_accessor :user, :vehicle
 
 	def user
 		@user = User.find_by_id(self.user_id)
+	end
+
+	def vehicle
+		@vehicle = User.find_by_id(self.vehicle_id)
 	end
 end
