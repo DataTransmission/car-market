a97e29bf618bd7fb347bbb7197c2b96463d3f22f
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-02 00:33:17 -0700

    add show method to vehicles_controller;

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -9,6 +9,12 @@ class Api::V1::VehiclesController < ApplicationController
 		end
 	end
 
+	def show
+		respond_to do |format|
+			format.json { render json: @vehicle }
+		end
+	end
+
 	private
     def fetch_vehicle
     	@vehicle = Vehicle.find_by_id(params[:id])
