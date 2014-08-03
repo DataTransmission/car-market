81407ccdbb8fde657fd80ff2584b444fda67969f
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-03 13:27:27 -0700

    (1) add attr_accessor for photos and listings on model of vehicle; (2) add dependent to vehicle on its relationship with listing;

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,10 +1,19 @@
 class Vehicle < ActiveRecord::Base
 	has_one :user
-	has_one :listing
+	has_one :listing, dependent: :destroy
 	has_many :photo
-	attr_accessor :user 
+	belongs_to :user
+	attr_accessor :user, :photos, :listing
 
 	def user
 		@user = User.find_by_id(self.user_id)
+	end
+
+	def photos
+		@photos = Photo.where(:vehicle_id => self.id).all
+	end
+
+	def listing
+		@listings = Listing.find_by_vehicle_id(self.id)
 	end
 end
