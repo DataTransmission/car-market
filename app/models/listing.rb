feb5de2c3bd6673a2f2204580e8992f8c63862b5
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-03 13:14:10 -0700

    add attr_accessor for user and vehicle on model of listing;

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,16 @@ class Listing < ActiveRecord::Base
 	has_one :vehicle
 	belongs_to :user
 	belongs_to :vehicle
+
+	attr_accessor :user, :vehicle
+
+	def user
+		@user = User.find_by_id(self.user_id)
+	end
+
+	def vehicle
+		@vehicle = Vehicle.find_by_id(self.vehicle_id)
+	end
+
+
 end
