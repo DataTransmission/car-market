eff46f8bc7f491f6ebea648b843f794987afee56
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-03 13:25:51 -0700

    add attr_accessor for photos on model of listing;

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,15 +4,17 @@ class Listing < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :vehicle
 
-	attr_accessor :user, :vehicle
+	attr_accessor :user, :photos, :vehicle
 
 	def user
 		@user = User.find_by_id(self.user_id)
 	end
 
+	def photos
+		@photos = Photo.where(:vehicle_id => self.vehicle_id).all
+	end
+
 	def vehicle
 		@vehicle = Vehicle.find_by_id(self.vehicle_id)
 	end
-
-
 end
