8be499de2af0b18e8bb343015d12f133d9639b9d
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-04 00:17:43 -0700

    add index method to listings_controller;

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::ListingsController < ApplicationController
+	def index
+		@listings = Listing.all
+		respond_to do |format|
+			format.json { render json: @listings }
+		end
+	end
 end
