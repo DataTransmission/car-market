bd222f3f07a6a4868c5480a24efe542e804b31e8
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-04 00:25:38 -0700

    add attr_accessor for user and listing on model of deal;

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,2 +1,11 @@
 class Deal < ActiveRecord::Base
+	attr_accessor :user, :listing
+
+	def user
+		@user = User.find_by_id(self.user_id)
+	end
+
+	def listing
+		@listing = Listing.find_by_id(self.listing_id)
+	end
 end
