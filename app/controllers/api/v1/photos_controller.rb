197c205d272e6c67ce51c6fa5e3eb0d9ec583234
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-05 19:48:11 -0700

    add create method to photos_controller with strong parameter protection;

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -2,21 +2,38 @@ class Api::V1::PhotosController < ApplicationController
 
   before_filter :fetch_photo, :except => [:index, :create]
 
-	def index
-		@photos = Photo.all
-		respond_to do |format|
-			format.json { render json: @photos }
-		end
+  def index
+    @photos = Photo.all
+    respond_to do |format|
+     format.json { render json: @photos }
+   end
+ end
+
+ def show
+  respond_to do |format|
+    format.json { render json: @photo }
   end
 
-  def show
+  def create
+    @photo = Photo.new(create_photo_params)
     respond_to do |format|
-      format.json { render json: @photo }
+      if @photo.save
+        format.json { render json: @photo, status: :created }
+      else
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
   def fetch_photo
     @photo = Photo.find_by_id(params[:id])
+  end
+
+  private
+  def create_photo_params
+    params
+    .require(:photo)
+    .permit(:user_id, :vehicle_id, :url)
   end
 end
