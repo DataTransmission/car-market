1866254d3a6a533ca60438f146a8af9fad295bd9
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-05 19:15:58 -0700

    add fetch_listing method, before_filter, and show method on the listings_controller;

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,8 +1,22 @@
 class Api::V1::ListingsController < ApplicationController
+
+  before_filter :fetch_listing, :except => [:index, :create]
+
 	def index
 		@listings = Listing.all
 		respond_to do |format|
 			format.json { render json: @listings }
 		end
-	end
+  end
+
+  def show
+    respond_to do |format|
+      format.json { render json: @listing }
+    end
+  end
+
+  private
+  def fetch_listing
+    @listing = Listing.find_by_id(params[:id])
+  end
 end
