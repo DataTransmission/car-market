452f8ba33d1ce0f2cdb2e6a460a88e66ebd5896a
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-05 19:22:37 -0700

    add fetch_deal method, before_filter, and show method on the deals_controller;

--- app/controllers/api/v1/deals_controller.rb
+++ app/controllers/api/v1/deals_controller.rb
@@ -1,8 +1,21 @@
 class Api::V1::DealsController < ApplicationController
-	def index
+  before_filter :fetch_deal, :except => [:index, :create]
+
+  def index
 		@deals = Deal.all
 		respond_to do |format|
 			format.json { render json: @deals }
 		end
-	end
+  end
+
+  def show
+    respond_to do |format|
+      format.json { render json: @deal }
+    end
+  end
+
+  private
+  def fetch_deal
+    @deal = Deal.find_by_id(params[:id])
+  end
 end
