e5daaef6181c8c4007b45d9b80b71588741338a9
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-05 19:11:52 -0700

    add fetch_photo method, before_filter, and show method on the photos_controller;

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,8 +1,22 @@
 class Api::V1::PhotosController < ApplicationController
+
+  before_filter :fetch_photo, :except => [:index, :create]
+
 	def index
 		@photos = Photo.all
 		respond_to do |format|
 			format.json { render json: @photos }
 		end
-	end
+  end
+
+  def show
+    respond_to do |format|
+      format.json { render json: @photo }
+    end
+  end
+
+  private
+  def fetch_photo
+    @photo = Photo.find_by_id(params[:id])
+  end
 end
