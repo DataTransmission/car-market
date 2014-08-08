9307a534c728d3d8e524348bb8c69c76e21cd6a7
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-08 00:19:53 -0700

    update seeds;

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,61 +28,67 @@ for i in (1..number_of_users)
 	@user = User.where(email: email).first
 	if @user == nil
 		User.create!(
-			first_name: fname,
-			last_name: lname,
-			nickname: "#{lname}.#{digit}",
-			email: email,
-			password: "000000",
-			password_confirmation: "000000",
-			status: 'active'
-			)
+		first_name: fname,
+		last_name: lname,
+		nickname: "#{lname}.#{digit}",
+		email: email,
+		password: "000000",
+		password_confirmation: "000000",
+		status: 'active'
+		)
 	end
 end
 
 for i in (0..9)
+	vin = "19XFA1E50BE00000#{i}"
 	Vehicle.create!(
-		vin: "19XFA1E50BE00000#{i}",
-		manufacturer: 'Honda',
-		model: 'Civic',
-		year: '2011',
-		color: '',
-		user_id: (1..number_of_users).to_a.shuffle[0,1].join
-		)
-	Vehicle.create!(
-		vin: "19XFB2F51DE00000#{i}",
-		manufacturer: 'Honda',
-		model: 'Civic',
-		year: '2013',
-		color: '',
-		user_id: (1..number_of_users).to_a.shuffle[0,1].join
-		)
+	vin: vin,
+	manufacturer: 'Honda',
+	model: 'Civic',
+	year: '2011',
+	color: '',
+	user_id: (1..number_of_users).to_a.shuffle[0,1].join
+	)
 
+	vin = "19XFB2F51DE00000#{i}"
 	Vehicle.create!(
-		vin: "2HNYD2H20CH00000#{i}",
-		manufacturer: 'Acura',
-		model: 'MDX',
-		year: '2012',
-		color: '',
-		user_id: (1..number_of_users).to_a.shuffle[0,1].join
-		)
+	vin: vin,
+	manufacturer: 'Honda',
+	model: 'Civic',
+	year: '2013',
+	color: '',
+	user_id: (1..number_of_users).to_a.shuffle[0,1].join
+	)
 
+	vin = "2HNYD2H20CH00000#{i}"
 	Vehicle.create!(
-		vin: "2HNYD2H23CH00000#{i}",
-		manufacturer: 'Acura',
-		model: 'MDX',
-		year: '2012',
-		color: '',
-		user_id: (1..number_of_users).to_a.shuffle[0,1].join
-		)
+	vin: vin,
+	manufacturer: 'Acura',
+	model: 'MDX',
+	year: '2012',
+	color: '',
+	user_id: (1..number_of_users).to_a.shuffle[0,1].join
+	)
 
+	vin = "2HNYD2H23CH00000#{i}"
 	Vehicle.create!(
-		vin: "2G1SCHED4C900000#{i}",
-		manufacturer: 'Chevrolet',
-		model: 'Suburban',
-		year: '2012',
-		color: '',
-		user_id: (1..number_of_users).to_a.shuffle[0,1].join
-		)
+	vin: vin,
+	manufacturer: 'Acura',
+	model: 'MDX',
+	year: '2012',
+	color: '',
+	user_id: (1..number_of_users).to_a.shuffle[0,1].join
+	)
+
+	vin = "2G1SCHED4C900000#{i}"
+	Vehicle.create!(
+	vin: vin,
+	manufacturer: 'Chevrolet',
+	model: 'Suburban',
+	year: '2012',
+	color: '',
+	user_id: (1..number_of_users).to_a.shuffle[0,1].join
+	)
 end
 
 
