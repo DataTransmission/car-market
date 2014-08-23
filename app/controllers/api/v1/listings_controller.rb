1f8ea6745fef68bd897dad4cb5ac4fc7604a5eb7
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-22 18:19:10 -0700

    add filter_listing() method;

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -39,4 +39,22 @@ class Api::V1::ListingsController < ApplicationController
   def fetch_listing
     @listing = Listing.find_by_id(params[:id])
   end
+
+  private
+  def filter_listing(listing)
+    hash = Hash.new()
+    hash[:price] = listing.price
+    hash[:currency] = listing.currency
+    hash[:status] = listing.status
+    hash[:city] = listing.city
+    hash[:state] = listing.state
+    hash[:country] = listing.country
+    hash[:zip_code] = listing.zip_code
+    hash[:description] = listing.description
+    hash[:created_at] = listing.created_at
+    hash[:updated_at] = listing.updated_at
+    hash[:vehicle] = listing.vehicle
+
+    return hash
+  end
 end
