12b008b5bd8270ee36b022888b2ed3786564d354
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-22 18:13:59 -0700

    update index() method on listings_controller;

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -4,8 +4,28 @@ class Api::V1::ListingsController < ApplicationController
 
 	def index
     @listings = Listing.offset(rand(Listing.count)).limit(5)
+
+    result = Array.new()
+    for listing in @listings
+      hash = Hash.new()
+      hash[:price] = listing.price
+      hash[:currency] = listing.currency
+      hash[:status] = listing.status
+      hash[:city] = listing.city
+      hash[:state] = listing.state
+      hash[:country] = listing.country
+      hash[:zip_code] = listing.zip_code
+      hash[:description] = listing.description
+      hash[:created_at] = listing.created_at
+      hash[:updated_at] = listing.updated_at
+      hash[:vehicle] = listing.vehicle
+
+      result.push(hash)
+    end
+
+
 		respond_to do |format|
-			format.json { render json: @listings }
+			format.json { render json: result }
 		end
   end
 
