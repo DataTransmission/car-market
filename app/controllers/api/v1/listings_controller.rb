5aab0f86aac744f8d31486290b61d4332cdc2f23
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-22 18:20:29 -0700

    implement filter_listing() method on index() and show() methods on listings_controller;

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -2,36 +2,22 @@ class Api::V1::ListingsController < ApplicationController
 
   before_filter :fetch_listing, :except => [:index, :create]
 
-	def index
+  def index
     @listings = Listing.offset(rand(Listing.count)).limit(5)
 
     result = Array.new()
     for listing in @listings
-      hash = Hash.new()
-      hash[:price] = listing.price
-      hash[:currency] = listing.currency
-      hash[:status] = listing.status
-      hash[:city] = listing.city
-      hash[:state] = listing.state
-      hash[:country] = listing.country
-      hash[:zip_code] = listing.zip_code
-      hash[:description] = listing.description
-      hash[:created_at] = listing.created_at
-      hash[:updated_at] = listing.updated_at
-      hash[:vehicle] = listing.vehicle
-
-      result.push(hash)
+      result.push(filter_listing(listing))
     end
 
-
-		respond_to do |format|
-			format.json { render json: result }
-		end
+    respond_to do |format|
+      format.json { render json: result }
+    end
   end
 
   def show
     respond_to do |format|
-      format.json { render json: @listing }
+      format.json { render json: filter_listing(@listing) }
     end
   end
 
