d46fad097e4b6425b352eabb03ae88316fe89814
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-03 21:01:05 -0700

    add create() method to vehicles_controller;

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -21,6 +21,36 @@ class Api::V1::VehiclesController < ApplicationController
 		end
 	end
 
+	def create
+		msg = Hash.new
+
+		@vehicle = Vehicle.new(create_vehicle_params)
+
+		if @vehicle.save
+			msg[:vehicle] = @vehicle
+			msg[:status] = 200
+			msg[:message] = ''
+		else
+
+			str = ""
+			errors = @vehicle.errors
+			for key in errors
+				for error in errors[key]
+					str += "#{key} #{error}" + "\n"
+				end
+			end
+
+			msg[:status] = 422
+			msg[:message] = str[0..-2]
+
+		end
+
+
+		respond_to do |format|
+			format.json { render json: msg }
+		end
+	end
+
 	private
     def fetch_vehicle
     	@vehicle = Vehicle.find_by_id(params[:id])
