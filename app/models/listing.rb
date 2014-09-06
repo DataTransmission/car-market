fd2dfc2598a6ed112018ce7bd3131c917e57a26e
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-06 00:39:15 -0700

    add validations to listing model;

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,15 @@ class Listing < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :vehicle
 
+	validates(:user_id, presence: true)
+	validates(:vehicle_id, presence: true, uniqueness: true)
+	validates(:price, presence: true)
+	validates(:currency, presence: true)
+	validates(:status, presence: true)
+	validates(:city, presence: true)
+	validates(:state, presence: true)
+	validates(:country, presence: true)
+	validates(:zip_code, presence: true)
+	validates(:description, presence: true)
+
 end
